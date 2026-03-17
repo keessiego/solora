109a746f976b24f9733b19438acf9992ae35e4a3
keessiego <lourens@kerkhoff-ict.nl>
2026-03-17 12:50:43 +0100

    Sidbar fixes, eerste goed werkende versie

--- docs/sidebar.php
+++ docs/sidebar.php
@@ -1,16 +1,16 @@
 <?php 
-$currentPage = basename($_SERVER['SELF_PHP']);
+$currentPage = basename($_SERVER['PHP_SELF']);
 ?>
-<div class="flex flex-col h-full w-82 p-3 border-r bg-zinc-100 border-zinc-300">
+<div class="flex flex-col h-full w-82 p-3 border-r bg-zinc-100 border-zinc-300 min-w-xs">
   <button
     onclick="window.location.href = 'install.php'"
-    class="btn-secondary btn-sm text-left w-full active rounded-full my-1"
+    class="btn-secondary btn-sm text-left w-full <?= $currentPage === 'install.php' ? 'active' : '' ?> rounded-full my-1"
   >
     Get Started
   </button>
   <button
     onclick="window.location.href = 'buttons.php'"
-    class="btn-secondary btn-sm text-left w-full rounded-full my-1"
+    class="btn-secondary btn-sm text-left w-full <?= $currentPage === 'buttons.php' ? 'active' : '' ?> rounded-full my-1"
   >
     Buttons
   </button>
